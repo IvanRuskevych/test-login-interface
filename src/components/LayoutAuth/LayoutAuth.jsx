e2884c142a9bd4c2e17c030196dff158fcfbf94e
Ivan <ivan.ruskevych@gmail.com>
2024-03-18 22:06:24 +0200

    Revert "fix: path to icon"

--- src/components/LayoutAuth/LayoutAuth.jsx
+++ src/components/LayoutAuth/LayoutAuth.jsx
@@ -1,14 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import { AuthContainer, IconLogo, IconWrapper, Section } from './LayoutAuth.styled.js';
-// import logo from '../../assets/icons/logo.svg';
-import vite from"../../../public/vite.svg"
+import logo from '../../assets/icons/logo.svg';
 
 const LayoutAuth = () => {
   return (
     <AuthContainer>
       <Section>
         <IconWrapper>
-          <IconLogo src={vite} alt="icon logo" />
+          <IconLogo src={logo} alt="icon logo" />
         </IconWrapper>
         <Outlet />
       </Section>
