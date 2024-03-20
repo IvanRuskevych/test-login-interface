259baac897348bdbe9b96c3e7c2228763c63959c
IvanRuskevych <ivan.ruskevych@gmail.com>
2024-03-20 13:17:25 +0200

    add: password reset api & errors

--- src/components/forms/FormPasswordReset.jsx
+++ src/components/forms/FormPasswordReset.jsx
@@ -4,24 +4,37 @@ import FormButton from '../FormButton/FormButton.jsx';
 import LinkCancel from '../links/LinkCancel.jsx';
 import {useState} from "react";
 import {toast, ToastContainer} from 'react-toastify';
+import {setAccessToken, setRefreshToken} from "../../utils/tokens.js";
+import {useApiPostRequest} from "../../hooks/apiRequests.js";
 
 const FormPasswordReset = () => {
-    const [formData, setFromData] = useState({
+    const [formData, setFormData] = useState({
         email: ""
     })
+    const {data, sendData} = useApiPostRequest()
 
     const handleChange = (e) => {
         const {name, value} = e.target
-        setFromData({...formData, [name]: value})
+        setFormData({...formData, [name]: value})
     }
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault()
         const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i
         if (formData.email.length === 0) {
             toast.error("Enter email");
         } else if (!regexEmail.test(formData.email)) {
             toast.error("This is not a valid email");
+        } else if (regexEmail.test(formData.email)) {
+            try {
+                await sendData("/v1/auth/password-reset", formData);
+                setAccessToken(data.access_token);
+                setRefreshToken(data.refresh_token);
+                setFormData({email: "",});
+                toast.success(`Please check your email ${formData.email} to complete the password reset process`);
+            } catch (error) {
+                toast.error("Failed to reset password. Please try again later.");
+            }
         }
     }
 
@@ -29,7 +42,8 @@ const FormPasswordReset = () => {
         <>
             <ToastContainer/>
             <Form onSubmit={handleSubmit}>
-                <FieldEmail value={formData.email} onChange={handleChange} placeholder={'Enter your email'} className={"field-email"}/>
+                <FieldEmail value={formData.email} onChange={handleChange} placeholder={'Enter your email'}
+                            className={"field-email"}/>
                 <FormButton type={'submit'} text={'Send'}/>
             </Form>
             <LinkCancel href={'v1/auth/login'} text={'Cancel'} className={'link-cancel'}/>
